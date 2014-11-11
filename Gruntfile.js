5e5a698a2b00298c436a0e0bf6ea12841f229af5
Rupert Angermeier <rupert.angermeier@gmail.com>
2014-11-11 11:17:12 +0100

    config grunt bump plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,7 +98,7 @@ module.exports = function (grunt) {
     bump: {
       options: {
         files: ['package.json', 'bower.json'],
-        commitFiles: ['package.json', 'bower.json'],
+        commitFiles: ['package.json', 'bower.json', '*.md', 'dist/*'],
         pushTo: 'origin'
       }
     }
